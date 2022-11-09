1e6932f2e1d6579a86291059aa2dda6667ce91e9
Hamed Xamani <xamanihamed@gmail.com>
2022-11-09 15:24:15 +0330

    Add Post and Comment Models

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,15 +2,14 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
-class User extends Authenticatable
+class User extends Model
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasApiTokens, HasFactory;
 
     /**
      * The attributes that are mass assignable.
@@ -41,4 +40,14 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function posts()
+    {
+        $this->hasMany(Post::class);
+    }
+
+    public function comments()
+    {
+        $this->hasMany(Comment::class);
+    }
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Comment;
+use App\Models\Post;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -16,9 +19,13 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        $users = User::factory(3)->create();
+        $posts = Post::factory(3)->make()->each(function($post) use ($users) {
+            $post->user_id = $users->random()->id;
+        });
+        Comment::factory(3)->make()->each(function ($comment) use ($users, $posts){
+            $comment->user_id = $users->random()->id;
+            $comment->post_id = $posts->random()->id;
+        });
     }
 }
